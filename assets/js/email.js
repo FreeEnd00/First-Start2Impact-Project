ff0c0466bfb5e94582f5405eb25281af91c56a81
FreeEnd00 <thomas.ginder00@gmail.com>
2024-03-13 12:37:13 +0100

    email.js test

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,21 +1,8 @@
 function sendMail() {
-    var params = {
+    let parms = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_nktihpg";
-    const templateID ="template_i5xcozj";
-
-    emailjs
-        .send(serviceID,templateID,params)
-        .then((res) => {
-         document.getElementById("name").value = "";
-         document.getElementById("email").value = "";
-         document.getElementById("message").value = "";
-         console.log(res);
-         alert("your message sent successfully");
-        })
-        .chatch((err) => console.log(err));
+    }
+    emailjs.send("service_nktihpg","template_i5xcozj",parms).then(alert("Email Sent Successfully!"))
 }
