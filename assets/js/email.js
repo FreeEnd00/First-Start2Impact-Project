3a61b83e5e3cbd8d70d3a5743339e576617810f4
FreeEnd00 <thomas.ginder00@gmail.com>
2024-03-14 20:40:14 +0100

    working form without email js

--- assets/js/email.js
+++
@@ -1,8 +0,0 @@
-function sendMail() {
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    }
-    emailjs.send("service_nktihpg","template_i5xcozj",parms).then(alert("Email Sent Successfully!"))
-}
